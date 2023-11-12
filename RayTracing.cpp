0045cd8e2d7ec02bafcaff638c50525ce089615a
ThePlatypusEveryoneLoves <atharv.sondhi@gmail.com>
2023-11-12 17:47:27 -0600

    Simplified the Quadratic formula by factoring out 2

--- RayTracing.cpp
+++ RayTracing.cpp
@@ -5,17 +5,17 @@
 #include "Vec3.h"
 #include "Ray.h"
 
-double hit_sphere(const Point3& center, double radius, const ray& r) {
+double hit_sphere(const point3& center, double radius, const ray& r) {
     vec3 oc = r.origin() - center;
-    auto a = dot(r.direction(), r.direction());
-    auto b = 2.0 * dot(oc, r.direction());
-    auto c = dot(oc, oc) - radius*radius;
-    auto discriminant = b*b - 4*a*c;
+    auto a = r.direction().length_squared();
+    auto half_b = dot(oc, r.direction());
+    auto c = oc.length_squared() - radius*radius;
+    auto discriminant = half_b*half_b - a*c;
 
     if (discriminant < 0) {
         return -1.0;
     } else {
-        return (-b - sqrt(discriminant) ) / (2.0*a);
+        return (-half_b - sqrt(discriminant) ) / a;
     }
 }
 
